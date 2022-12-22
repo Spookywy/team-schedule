09f52872dc2f15c61392aa421459955c818b01e0
Valentin Menoret <109912731+Spookywy@users.noreply.github.com>
2022-12-22 15:11:54 +0100

    :fire: remove code

---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Team Schedule</title>
+        <meta name="description" content="Team Schedule is a small staff scheduling app." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>Team Schedule</h1>
+      </main>
+    </>
+  )
+}
